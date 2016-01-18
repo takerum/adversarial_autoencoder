5d94becfdf881b84ba621866360754f2bd871417
Takeru Miyato <miyato-t@maui00.1017345.kuins.net>
2016-01-18 14:34:24 +0900

    change the symbols of latent variables

--- source/costs/adversarial_autoenc_loss.py
+++ source/costs/adversarial_autoenc_loss.py
@@ -4,23 +4,23 @@ import numpy
 from binary_cross_entropy_loss import _binary_cross_entropy_loss
 from quadratic_loss import _quadratic_loss
 
-def adversarial_autoenc_loss(x,enc_f,dec_f,disc_f,q_sampler,
+def adversarial_autoenc_loss(x,enc_f,dec_f,disc_f,p_z_sampler,
                              obj_type,
                              lamb=numpy.asarray(1.0,dtype=theano.config.floatX)):
 
-    z_p = enc_f(x)
-    z_q = q_sampler(z_p)
+    z_q = enc_f(x)
+    z_p = p_z_sampler(z_q)
 
-    adv_loss = adversarial_loss(z_p=z_p ,z_q=z_q ,disc_f=disc_f)
-    recon_loss = reconstruction_loss(x=x ,z=z_p ,dec_f=dec_f ,obj_type=obj_type)
+    adv_loss = adversarial_loss(z_q=z_q , z_p=z_p ,disc_f=disc_f)
+    recon_loss = reconstruction_loss(x=x ,z=z_q ,dec_f=dec_f ,obj_type=obj_type)
 
     return recon_loss - lamb*adv_loss, recon_loss, adv_loss
 
 
-def adversarial_loss(z_q,z_p,disc_f):
-    y_p = disc_f(z_p)
+def adversarial_loss(z_p,z_q,disc_f):
     y_q = disc_f(z_q)
-    return -T.mean(T.log(y_q) + (T.log(1-y_p)))
+    y_p = disc_f(z_p)
+    return -T.mean(T.log(y_p) + (T.log(1-y_q)))
 
 def reconstruction_loss(x,z,dec_f,obj_type='QE'):
     x_ = dec_f(z)
